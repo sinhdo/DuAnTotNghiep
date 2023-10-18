8f9ccf6bdb367e0f594230a59b570148175489ee
Pham_Quang_Thang <thangpqph27877@fpt.edu.vn>
2023-10-18 10:31:59 +0700

    PF_fragment

--- app/src/main/java/com/example/duantotnghiep/fragment/HomeFragment.java
+++ app/src/main/java/com/example/duantotnghiep/fragment/HomeFragment.java
@@ -12,21 +12,27 @@ import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.duantotnghiep.R;
 import com.example.duantotnghiep.adapter.ProductHomeAdapter;
 import com.example.duantotnghiep.adapter.SliderImageAdapter;
-import com.google.firebase.database.core.Context;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class HomeFragment extends Fragment { {
-        // Required empty public constructor
-    }
+public class HomeFragment extends Fragment {
+    private TextView textViewName;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference mReference;
 
     public static HomeFragment newInstance() {
         HomeFragment fragment = new HomeFragment();
@@ -36,66 +42,85 @@ public class HomeFragment extends Fragment { {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Handler handler = new Handler();
-
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        textViewName = view.findViewById(R.id.txtName);
         RecyclerView recyclerView = view.findViewById(R.id.view1);
         RecyclerView recyclerView2 = view.findViewById(R.id.view2);
 
         ProductHomeAdapter productHomeAdapter = new ProductHomeAdapter(getContext());
         recyclerView.setAdapter(productHomeAdapter);
         recyclerView2.setAdapter(productHomeAdapter);
-        LinearLayoutManager layoutManager2 = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false);
+        LinearLayoutManager layoutManager2 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
         ViewPager2 viewPager2 = view.findViewById(R.id.viewPaperSlider);
 
         viewPager2.setAdapter(new SliderImageAdapter(getContext()));
-viewPager2.setClipToPadding(false);
-viewPager2.setClipChildren(false);
-viewPager2.setOffscreenPageLimit(5);
-viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(5);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
         CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
         compositePageTransformer.addTransformer(new MarginPageTransformer(40));
         compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
             @Override
             public void transformPage(@NonNull View page, float position) {
-                float r = 1-Math.abs(position);
-                page.setScaleY(0.85f + r*0.15f);
-
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
             }
         });
         viewPager2.setPageTransformer(compositePageTransformer);
 
-
         recyclerView.setLayoutManager(layoutManager);
         recyclerView2.setLayoutManager(layoutManager2);
+
+        Handler handler = new Handler();
         Runnable sliderRunnable = new Runnable() {
             @Override
             public void run() {
-                viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+                viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
             }
         };
+
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
                 handler.removeCallbacks(sliderRunnable);
-                handler.postDelayed(sliderRunnable,3000);
+                handler.postDelayed(sliderRunnable, 3000);
             }
         });
 
         return view;
     }
 
+    private void setInfoProfile() {
+        String id = firebaseUser.getUid();
+        DatabaseReference userRef = mReference.child("user").child(id);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String name = snapshot.child("username").getValue(String.class);
+                textViewName.setText(name);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Loi", "onCancelled: " + error.getMessage());
+            }
+        });
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        setInfoProfile();
     }
 }
